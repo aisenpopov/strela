c5902289d21303c3765258c4bfff1380f05b1e34
aisenpopov <aisen.popov@gmail.com>
2016-05-22 16:06:14 +0300

    fix athlete search

--- src/main/java/ru/strela/repository/spec/AthleteSpec.java
+++ src/main/java/ru/strela/repository/spec/AthleteSpec.java
@@ -1,15 +1,13 @@
 package ru.strela.repository.spec;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.persistence.criteria.*;
-
 import org.apache.commons.lang.StringUtils;
 import org.springframework.data.jpa.domain.Specification;
-
 import ru.strela.model.Athlete;
 import ru.strela.model.filter.AthleteFilter;
+
+import javax.persistence.criteria.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class AthleteSpec {
     public static Specification<Athlete> filter(final AthleteFilter filter) {
@@ -19,12 +17,20 @@ public class AthleteSpec {
                 List<Predicate> predicates = new ArrayList<Predicate>();
 
                 if(StringUtils.isNotBlank(filter.getQuery())) {
-                    Expression<String> displayName = builder.concat(root.get("lastName").as(String.class), " ");
-                    displayName = builder.concat(displayName, root.get("firstName").as(String.class));
-                    displayName = builder.concat(displayName, " ");
-                    displayName = builder.concat(displayName, root.get("middleName").as(String.class));
-                    predicates.add(builder.like(builder.lower(displayName),
-                            "%" + filter.getQuery().toLowerCase() + "%"));
+                    Expression<String> firstName = root.get("firstName").as(String.class);
+                    Expression<String> lastName = root.get("lastName").as(String.class);
+                    Expression<String> middleName = root.get("middleName").as(String.class);
+
+                    Expression<String> displayNameFull = builder.concat(lastName, " ");
+                    displayNameFull = builder.concat(displayNameFull, firstName);
+                    displayNameFull = builder.concat(displayNameFull, " ");
+                    displayNameFull = builder.concat(displayNameFull, middleName);
+
+                    Expression<String> displayName = builder.concat(lastName, " ");
+                    displayName = builder.concat(displayName, firstName);
+
+                    predicates.add(builder.or(builder.and(builder.like(builder.lower(displayNameFull), "%" + filter.getQuery().toLowerCase() + "%"), builder.isNotNull(middleName)),
+                                              builder.and(builder.like(builder.lower(displayName), "%" + filter.getQuery().toLowerCase() + "%"), builder.isNull(middleName))));
                 }
                 if (filter.getInstructor() != null) {
                     predicates.add(builder.equal(root.get("instructor"), filter.getInstructor()));
