646fcf5cbfa57892d943333b7d836d8a017379c2
popov <aisen.popov.asap@gmail.com>
2016-10-23 23:25:25 +0300

    news html content render fix

--- src/main/webapp/resources/js/web/NewsCtrls.js
+++ src/main/webapp/resources/js/web/NewsCtrls.js
@@ -27,12 +27,13 @@ app.controller("NewsListCtrl", function ($scope, CommonService, $location) {
 
 });
 
-app.controller("NewsCtrl", function ($scope, CommonService, $routeParams, $rootScope) {
+app.controller("NewsCtrl", function ($scope, CommonService, $routeParams, $rootScope, $sce) {
 
     $scope.news = {};
     CommonService.post("/news/" + $routeParams.path).then(function (resp) {
         var data = resp.data.data;
         if (data) {
+            data.text = $sce.trustAsHtml(data.text);
             $scope.news = data;
             $rootScope.htmlTitle = data.name;
         }
@@ -40,12 +41,13 @@ app.controller("NewsCtrl", function ($scope, CommonService, $routeParams, $rootS
 
 });
 
-app.controller("StaticPageCtrl", function ($scope, CommonService, $routeParams, $rootScope) {
+app.controller("StaticPageCtrl", function ($scope, CommonService, $routeParams, $rootScope, $sce) {
 
     $scope.staticPage = {};
     CommonService.post("/static/" + $routeParams.path).then(function (resp) {
         var data = resp.data.data;
         if (data) {
+            data.text = $sce.trustAsHtml(data.text);
             $scope.staticPage = data;
             $rootScope.htmlTitle = data.name;
         }
